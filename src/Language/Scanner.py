fc7486f7947c70aa539b2bc8c5942df0b5936162
bryan967132 <bryan.tejaxun@gmail.com>
2023-08-25 13:34:51 -0600

    Analizador Lexico

--- src/Language/Scanner.py
+++ src/Language/Scanner.py
@@ -32,41 +32,9 @@ reserveds = {
     'loss'    : 'RW_loss',
     'execute' : 'RW_execute',
     'rep'     : 'RW_rep',
-    'b'       : 'TK_unit',
-    'k'       : 'TK_unit',
-    'm'       : 'TK_unit'
 }
 
-tokens = (
-    'RW_mkdisk',
-    'RW_rmdisk',
-    'RW_fdisk',
-    'RW_mount',
-    'RW_unmount',
-    'RW_mkfs',
-    'RW_login',
-    'RW_logout',
-    'RW_mkgrp',
-    'RW_rmgrp',
-    'RW_mkusr',
-    'RW_rmusr',
-    'RW_mkfile',
-    'RW_cat',
-    'RW_remove',
-    'RW_edit',
-    'RW_rename',
-    'RW_mkdir',
-    'RW_copy',
-    'RW_move',
-    'RW_find',
-    'RW_chown',
-    'RW_chgrp',
-    'RW_chmod',
-    'RW_pause',
-    'RW_recovery',
-    'RW_loss',
-    'RW_execute',
-    'RW_rep',
+tokens = tuple(reserveds.values()) + (
     'RW_size',
     'RW_path',
     'RW_fit',
@@ -100,35 +68,6 @@ m = {
     'ID2': r'[a-zA-Z0-9_][a-zA-Z0-9_\s]*',
 }
 
-t_RW_mkdisk   = r'(M|m)(K|k)(D|d)(I|i)(S|s)(K|k)'
-t_RW_rmdisk   = r'(R|r)(M|m)(D|d)(I|i)(S|s)(K|k)'
-t_RW_fdisk    = r'(F|f)(D|d)(I|i)(S|s)(K|k)'
-t_RW_mount    = r'(M|m)(O|o)(U|u)(N|n)(T|t)'
-t_RW_unmount  = r'(U|u)(N|n)(M|m)(O|o)(U|u)(N|n)(T|t)'
-t_RW_mkfs     = r'(M|m)(K|k)(F|f)(S|s)'
-t_RW_login    = r'(L|l)(O|o)(G|g)(I|i)(N|n)'
-t_RW_logout   = r'(L|l)(O|o)(G|g)(O|o)(U|u)(T|t)'
-t_RW_mkgrp    = r'(M|m)(K|k)(G|g)(R|r)(P|p)'
-t_RW_rmgrp    = r'(R|r)(M|m)(G|g)(R|r)(P|p)'
-t_RW_mkusr    = r'(M|m)(K|k)(U|u)(S|s)(R|r)'
-t_RW_rmusr    = r'(R|r)(M|m)(U|u)(S|s)(R|r)'
-t_RW_mkfile   = r'(M|m)(K|k)(F|f)(I|i)(L|l)(E|e)'
-t_RW_cat      = r'(C|c)(A|a)(T|t)'
-t_RW_remove   = r'(R|r)(E|e)(M|m)(O|o)(V|v)(E|e)'
-t_RW_edit     = r'(E|e)(D|d)(I|i)(T|t)'
-t_RW_rename   = r'(R|r)(E|e)(N|n)(A|a)(M|m)(E|e)'
-t_RW_mkdir    = r'(M|m)(K|k)(D|d)(I|i)(R|r)'
-t_RW_copy     = r'(C|c)(O|o)(P|p)(Y|y)'
-t_RW_move     = r'(M|m)(O|o)(V|v)(E|e)'
-t_RW_find     = r'(F|f)(I|i)(N|n)(D|d)'
-t_RW_chown    = r'(C|c)(H|h)(O|o)(W|w)(N|n)'
-t_RW_chgrp    = r'(C|c)(H|h)(G|g)(R|r)(P|p)'
-t_RW_chmod    = r'(C|c)(H|h)(M|m)(O|o)(D|d)'
-t_RW_pause    = r'(P|p)(A|a)(U|u)(S|s)(E|e)'
-t_RW_recovery = r'(R|r)(E|e)(C|c)(O|o)(V|v)(E|e)(R|r)(Y|y)'
-t_RW_loss     = r'(L|l)(O|o)(S|s)(S|s)'
-t_RW_execute  = r'(E|e)(X|x)(E|e)(C|c)(U|u)(T|t)(E|e)'
-t_RW_rep      = r'(R|r)(E|e)(P|p)'
 t_RW_size     = r'\-\s*(S|s)(I|i)(Z|z)(E|e)'
 t_RW_path     = r'\-\s*(P|p)(A|a)(T|t)(H|h)'
 t_RW_fit      = r'\-\s*(F|f)(I|i)(T|t)'
@@ -148,14 +87,18 @@ t_RW_fileN    = r'\-\s*(F|f)(I|i)(L|l)(E|e)(N|n)'
 t_RW_destino  = r'\-\s*(D|d)(E|e)(S|s)(T|t)(I|i)(N|n)(O|o)'
 t_RW_ugo      = r'\-\s*(U|u)(G|g)(O|o)'
 t_RW_ruta     = r'\-\s*(R|r)(U|u)(T|t)(A|a)'
-t_TK_unit     = r'B|K|M|b|k|m'
 t_TK_number   = r'[0-9]+'
 t_TK_path     = rf'(((\.\.|\.)?\/)*{m["ID1"]})+(\.{m["EXT"]})|\"(((\.\.|\.)?\/)*{m["ID2"]})+(\.{m["EXT"]})\"'
 t_TK_equ      = r'\='
 
 def t_TK_id(t):
     r'[a-zA-Z_0-9]+'
-    t.type = reserveds.get(t.value.lower(), 'TK_id') 
+    if t.value.upper() in ['B', 'K', 'M']:
+        t.type = 'TK_unit'
+    elif not t.value.isdigit():
+        t.type = reserveds.get(t.value.lower(), 'TK_id')
+    else:
+        t.type = 'TK_number'
     return t
 
 def t_commentary(t):
