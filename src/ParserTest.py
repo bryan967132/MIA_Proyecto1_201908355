8b247286646c1ee96ea3cc4192c06afca3b391c7
bryan967132 <2780289260101@ingenieria.usac.edu.gt>
2023-08-26 19:32:12 -0600

    Ruta Relativa

--- src/ParserTest.py
+++ src/ParserTest.py
@@ -1,7 +1,7 @@
 from Language.Parser import *
 
 def parserTest():
-    input = 'execute -path = /home/bryan/Documentos/USAC/Archivos/2S2023/Laboratorio/Proyecto1/script.adsj'
+    input = 'execute -path = ../script.adsj'
     Scanner.lineno = 1
     e = parser.parse(input)
     e[0].exec(parser)
