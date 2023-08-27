0c678fe5e991f24e1302be8b30cde876b7f533d5
bryan967132 <bryan.tejaxun@gmail.com>
2023-08-26 18:53:56 -0600

    Main (Consola)

---
+++ src/Main.py
@@ -0,0 +1,15 @@
+from Language.Parser import *
+
+
+def menu():
+    print()
+    print('Proyecto 1')
+    while True:
+        execute = input('Command$ ')
+        if execute.lower() == 'exit':
+            break
+        Scanner.lineno = 1
+        exec = parser.parse(execute)
+        exec[0].exec(parser)
+
+menu()
