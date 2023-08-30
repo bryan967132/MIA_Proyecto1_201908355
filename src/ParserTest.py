28cc5d535d8a0bae31e1d4f7a5b8603c97a8e3da
bryan967132 <bryan.tejaxun@gmail.com>
2023-08-29 19:17:19 -0600

    parser test

--- src/ParserTest.py
+++ src/ParserTest.py
@@ -1,7 +1,7 @@
 from Language.Parser import *
 
 def parserTest():
-    input = 'execute -path = ../script.adsj'
+    input = 'execute -path = ../scriptFF.adsj'
     Scanner.lineno = 1
     e = parser.parse(input)
     e[0].exec(parser)
