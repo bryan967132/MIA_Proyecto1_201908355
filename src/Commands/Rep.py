ad7d8e0f25b3f7f3fea7f5290130b2029d7262ed
bryan967132 <bryan.tejaxun@gmail.com>
2023-08-31 02:02:15 -0600

    Particiones Logicas y Extendidas (Provisional)

--- src/Commands/Rep.py
+++ src/Commands/Rep.py
@@ -61,16 +61,16 @@ class Rep:
                             occupiedCells += int(space)
                             dotParts += f'\n\t\t\t\t<TD COLSPAN="{int(space)}" ROWSPAN="6">{mbr.partitions[i].name.strip()}<BR/>Primaria<BR/>{self.porcentaje(round(space / 2, 2))} %</TD>'
                         elif mbr.partitions[i].type == 'E':
-                            extendedParts = '\n\t\t<TR>'
+                            extendedParts = '\n\t\t\t<TR>'
                             file.seek(mbr.partitions[i].start)
                             ebr = EBR.decode(file.read(30))
                             lastNoEmptyByteExt = mbr.partitions[i].start + 30
                             occupiedExtend = 0
                             if ebr.status:
                                 while True:
-                                    extendedParts += '\n\t\t\t<TD COLSPAN="10" ROWSPAN="5">EBR</TD>'
+                                    extendedParts += '\n\t\t\t\t<TD COLSPAN="10" ROWSPAN="5">EBR</TD>'
                                     space = round((ebr.size / mbr.size) * 200, 2)
-                                    extendedParts += f'\n\t\t\t<TD COLSPAN="{int(space)}" ROWSPAN="5">{mbr.partitions[i].name.strip()}<BR/>Logica<BR/>{self.porcentaje(round(space / 2, 2))} %</TD>'
+                                    extendedParts += f'\n\t\t\t\t<TD COLSPAN="{int(space)}" ROWSPAN="5">{mbr.partitions[i].name.strip()}<BR/>Logica<BR/>{self.porcentaje(round(space / 2, 2))} %</TD>'
                                     lastNoEmptyByteExt = ebr.start + ebr.size - 1
                                     occupiedExtend += 10 + int(space)
                                     if ebr.next == -1:
@@ -79,12 +79,12 @@ class Rep:
                                     ebr = EBR.decode(file.read(30))
                             else:
                                 occupiedExtend += 10
-                                extendedParts += '\n\t\t\t<TD COLSPAN="10" ROWSPAN="5">EBR</TD>'
+                                extendedParts += '\n\t\t\t\t<TD COLSPAN="10" ROWSPAN="5">EBR</TD>'
                             if lastNoEmptyByteExt < mbr.partitions[i].start + mbr.partitions[i].size:
                                 space = round(((mbr.partitions[i].start + mbr.partitions[i].size - (lastNoEmptyByteExt + 1)) / mbr.size) * 200, 2)
-                                extendedParts += f'\n\t\t\t<TD COLSPAN="{int(space)}" ROWSPAN="5">Libre<BR/>{self.porcentaje(round(space / 2, 2))} %</TD>'
+                                extendedParts += f'\n\t\t\t\t<TD COLSPAN="{int(space)}" ROWSPAN="5">Libre<BR/>{self.porcentaje(round(space / 2, 2))} %</TD>'
                                 occupiedExtend += int(space)
-                            extendedParts += '\n\t\t</TR>'
+                            extendedParts += '\n\t\t\t</TR>'
                             occupiedCells += occupiedExtend
                             dotParts += f'\n\t\t\t\t<TD COLSPAN="{occupiedExtend}" ROWSPAN="1">{mbr.partitions[i].name.strip()}<BR/>Extendida</TD>'
                         lastNoEmptyByte = mbr.partitions[i].start + mbr.partitions[i].size - 1
