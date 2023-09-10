d9f18ffff6ce78118934352d11a2213bcd3bc456
bryan967132 <bryan.tejaxun@gmail.com>
2023-09-10 03:06:00 -0600

    SuperBloque

--- src/Structures/SuperBlock.py
+++ src/Structures/SuperBlock.py
@@ -5,8 +5,8 @@ class SuperBlock:
         filesystem_type: int = -1,
         inodes_count: int = -1,
         blocks_count: int = -1,
-        free_blocks_count: int = -1,
         free_inodes_count: int = -1,
+        free_blocks_count: int = -1,
         mtime: datetime = None,
         umtime: datetime = None,
         mnt_count: int = -1,
@@ -23,8 +23,8 @@ class SuperBlock:
         self.filesystem_type: int = filesystem_type
         self.inodes_count: int = inodes_count
         self.blocks_count: int = blocks_count
-        self.free_blocks_count: int = free_blocks_count
         self.free_inodes_count: int = free_inodes_count
+        self.free_blocks_count: int = free_blocks_count
         self.mtime: datetime = mtime
         self.umtime: datetime = umtime
         self.mnt_count: int = mnt_count
@@ -42,8 +42,8 @@ class SuperBlock:
         result_b = self.filesystem_type.to_bytes(4, byteorder='big', signed=True)
         result_b += self.inodes_count.to_bytes(4, byteorder='big', signed=True)
         result_b += self.blocks_count.to_bytes(4, byteorder='big', signed=True)
-        result_b += self.free_blocks_count.to_bytes(4, byteorder='big', signed=True)
         result_b += self.free_inodes_count.to_bytes(4, byteorder='big', signed=True)
+        result_b += self.free_blocks_count.to_bytes(4, byteorder='big', signed=True)
         result_b += int(self.mtime.timestamp()).to_bytes(4, byteorder='big', signed=True) if self.mtime else b'\x00' * 4
         result_b += int(self.umtime.timestamp()).to_bytes(4, byteorder='big', signed=True) if self.umtime else b'\x00' * 4
         result_b += self.mnt_count.to_bytes(4, byteorder='big', signed=True)
@@ -62,8 +62,8 @@ class SuperBlock:
         filesystem_type: int = int.from_bytes(data[:4], byteorder='big', signed=True)
         inodes_count: int = int.from_bytes(data[4:8], byteorder='big', signed=True)
         blocks_count: int = int.from_bytes(data[8:12], byteorder='big', signed=True)
-        free_blocks_count: int = int.from_bytes(data[12:16], byteorder='big', signed=True)
-        free_inodes_count: int = int.from_bytes(data[16:20], byteorder='big', signed=True)
+        free_inodes_count: int = int.from_bytes(data[12:16], byteorder='big', signed=True)
+        free_blocks_count: int = int.from_bytes(data[16:20], byteorder='big', signed=True)
         mtime: datetime = datetime.datetime.fromtimestamp(int.from_bytes(data[20:24], byteorder='big', signed=True)) if data[20:24] != b'\x00' * 4 else None
         umtime: datetime = datetime.datetime.fromtimestamp(int.from_bytes(data[24:28], byteorder='big', signed=True)) if data[24:28] != b'\x00' * 4 else None
         mnt_count: int = int.from_bytes(data[28:32], byteorder='big', signed=True)
@@ -80,8 +80,8 @@ class SuperBlock:
             filesystem_type,
             inodes_count,
             blocks_count,
-            free_blocks_count,
             free_inodes_count,
+            free_blocks_count,
             mtime,
             umtime,
             mnt_count,
@@ -103,8 +103,8 @@ class SuperBlock:
         return f'''filsystem_type: {self.filesystem_type}
 inodes_count: {self.inodes_count}
 blocks_count: {self.blocks_count}
-free_blocks_count: {self.free_blocks_count}
 free_inodes_count: {self.free_inodes_count}
+free_blocks_count: {self.free_blocks_count}
 mtime: {self.mtime}
 umtime: {self.umtime}
 mnt_count: {self.mnt_count}
