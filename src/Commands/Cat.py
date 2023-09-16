64e27893f58873311f6c14df25e8cf17c35234cd
bryan967132 <bryan.tejaxun@gmail.com>
2023-09-16 12:23:46 -0600

    Comandos (estructuras)

---
+++ src/Commands/Cat.py
@@ -0,0 +1,10 @@
+class Cat:
+    def __init__(self, line: int, column: int):
+        self.line = line
+        self.column = column
+
+    def setParams(self, files: list):
+        self.files = files
+
+    def exec(self):
+        pass
