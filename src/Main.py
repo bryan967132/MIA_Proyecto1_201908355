704263164ae51a08f23159367121064a4d6030f5
bryan967132 <bryan.tejaxun@gmail.com>
2023-09-20 20:58:28 -0600

    Aplicación de Consola

--- src/Main.py
+++ src/Main.py
@@ -4,14 +4,18 @@ import os
 def menu():
     os.system('clear')
     print()
-    print('Proyecto 1')
+    print('\n\033[34m Proyecto 1\033[0m')
     while True:
-        execute = input('Command$ ')
-        if execute.lower() == 'exit':
-            break
-        scanner.lineno = 1
-        exec = parser.parse(execute)
-        if type(exec[0]) == Execute:
-            exec[0].exec(parser)
+        try:
+            execute = input(f'\033[34m Command$ \033[0m')
+            if execute.lower() == 'exit':
+                break
+            scanner.lineno = 1
+            exec = parser.parse(execute)
+            if type(exec[0]) == Execute:
+                exec[0].exec(parser)
+        except:
+            pass
+        print()
 
 menu()
