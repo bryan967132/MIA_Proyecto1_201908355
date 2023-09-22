eec6c1d0a47d4d01665cc96a3632a33ee15e6eb7
bryan967132 <bryan.tejaxun@gmail.com>
2023-09-21 18:46:54 -0600

    cat

--- src/Commands/Cat.py
+++ src/Commands/Cat.py
@@ -1,3 +1,9 @@
+from Structures.Tree import *
+from Structures.MBR import *
+from Env.Env import *
+import datetime
+import os
+
 class Cat:
     def __init__(self, line: int, column: int):
         self.line = line
@@ -7,4 +13,27 @@ class Cat:
         self.files = files
 
     def exec(self):
-        pass
+        if currentLogged['User']:
+            if len(self.files) > 0:
+                with open(currentLogged['PathDisk'], 'rb') as file:
+                    readed_bytes = file.read(127)
+                    mbr = MBR.decode(readed_bytes)
+                    for i in range(len(mbr.partitions)):
+                        if mbr.partitions[i].status and mbr.partitions[i].name.strip() == currentLogged['Partition']:
+                            file.seek(mbr.partitions[i].start)
+                            superBlock = SuperBlock.decode(file.read(SuperBlock.sizeOf()))
+                            tree: Tree = Tree(superBlock, file)
+                            print(f'\033[33m -> cat: \033[0m')
+                            for i in self.files:
+                                print('\033[53m\t -> ({})\n{:<20}\033[0m'.format(i[1], tree.readFile(i[1])[0]).replace('\n', '\n\t'))
+                            return
+            else:
+                self.__printError(f' -> Error cat: No se incluyeron archivos.')
+        else:
+            self.__printError(f" -> Error cat: No hay ningún usuario loggeado actualmente.")
+
+    def __printError(self, text):
+        print(f"\033[31m{text} [{self.line}:{self.column}]\033[0m")
+
+    def __printSuccess(self, text):
+        print(f"\033[32m{text} [{self.line}:{self.column}]\033[0m")
